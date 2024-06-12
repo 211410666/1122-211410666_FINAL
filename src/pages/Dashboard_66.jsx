d648355e97747919408f958af2754bd2726f9060
211410666 <113106817+211410666@users.noreply.github.com>
2024-06-12 19:24:33 +0800

    Update Dashboard_66.jsx

--- src/pages/Dashboard_66.jsx
+++ src/pages/Dashboard_66.jsx
@@ -13,15 +13,19 @@ const Dashboard_66 = () => {
         console.error('Error fetching user data:', error);
       });
   }, []);
-  return (
-    <>
-      <h1>Final-2 Exam of Web Prog 2,2024</h1>
-      <p>Guest name : 劉泓伸</p>
-      <p>Guest ID : 211410666</p>
-      <p>Supabase email : {userData.email}</p>
-      <p>Supabase user id : {userData.id}</p>
-    </>
-  );
+  if (userData != []) {
+    return (
+      <>
+        <h1>Final-2 Exam of Web Prog 2,2024</h1>
+        <p>Guest name : 劉泓伸</p>
+        <p>Guest ID : 211410666</p>
+        <p>Supabase email : {userData.email}</p>
+        <p>Supabase user id : {userData.id}</p>
+      </>
+    );
+  } else {
+    return <>loading...</>;
+  }
 };
 
 export default Dashboard_66;
